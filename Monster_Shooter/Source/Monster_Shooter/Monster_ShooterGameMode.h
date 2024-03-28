f640a3249c885e59ffb5dee7b6122a167ff61977
Ander42069 <91216296+Ander42069@users.noreply.github.com>
2024-03-28 15:00:53 -0600

    Monster_Shooter Project

---
+++ Monster_Shooter/Source/Monster_Shooter/Monster_ShooterGameMode.h
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Monster_ShooterGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMonster_ShooterGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMonster_ShooterGameMode();
+
+};
+
+
+
